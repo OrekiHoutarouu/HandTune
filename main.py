aa3438bf5b05826d99ba27025169a958d7edd1f3
Samuel Campelo <samuelcampelo25@gmail.com>
2026-03-15 08:17:00 -0300

    Shows user webcam

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from modules import gesture, hand_tracker, utils, volume_controller, webcam
+import cv2
 
+capture = webcam.get_webcam()
+
+while True:
+    frame = webcam.get_frame(capture)
+    cv2.imshow("Webcam", frame)
+    
+    if cv2.waitKey(1) == ord("q"):
+        exit()
--- modules/webcam/__init__.py
+++ modules/webcam/__init__.py
@@ -1,1 +1,15 @@
 import cv2
+
+def get_webcam():
+    capture = cv2.VideoCapture(0)
+
+    if not capture.isOpened():
+        exit()
+
+    return capture
+
+def get_frame(capture):
+    success, frame = capture.read()
+
+    if success:
+        return frame
